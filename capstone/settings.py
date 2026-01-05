6aad9b8dba67f131d5d7174cc49daa4fc8c8a552
Kyen Mangandi <kpmangandi08@gmail.com>
2026-01-05 22:20:34 +0800

    adjustments

--- capstone/settings.py
+++ capstone/settings.py
@@ -69,7 +69,7 @@ AUTH_USER_MODEL = 'applications.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

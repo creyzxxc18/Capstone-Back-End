fb9d404b616c66bea9e0abd44b366ac893bd8034
Kyen Mangandi <kpmangandi08@gmail.com>
2026-01-05 22:52:43 +0800

    adjustmentss

--- build.sh
+++ build.sh
@@ -6,5 +6,16 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Add this line temporarily to create your admin
-python manage.py createsuperuser --no-input || true
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+import os; \
+username = os.getenv('DJANGO_SUPERUSER_USERNAME'); \
+email = os.getenv('DJANGO_SUPERUSER_EMAIL'); \
+password = os.getenv('DJANGO_SUPERUSER_PASSWORD'); \
+first_name = os.getenv('DJANGO_SUPERUSER_FIRST_NAME'); \
+last_name = os.getenv('DJANGO_SUPERUSER_LAST_NAME'); \
+if not User.objects.filter(email=email).exists(): \
+    User.objects.create_superuser(email=email, username=username, password=password, first_name=first_name, last_name=last_name); \
+    print('Superuser created successfully!'); \
+else: \
+    print('Superuser already exists.')" | python manage.py shell
